7edb9fffc1f4ea38cbe8f87e5579bdbc96f7b0a3
Huynh Thi Khanh Linh <huynhthikhanhlinh2105@gmail.com>
2021-12-28 21:31:03 +0700

    connect api for sectionId

--- src/components/Information/Information.js
+++ src/components/Information/Information.js
@@ -1,16 +1,41 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Information.scss'
 import axios from 'axios'
 
 function Information() {
   const [title, setTitle] = useState('')
   const [description, setDescription] = useState('')
+  const [sectionId, setSectionId] = useState(0)
+  const [sectionNumber, setSectionNumber] = useState(1)
+  const [hasPrevious, setHasPrevious] = useState(false)
+  const [hasNext, setHasNext] = useState(false)
 
-  axios.get('https://gist.githubusercontent.com/bittermeatball/7854f3d7950469b0203a068fcaf27908/raw/1de87462c4f8c2fd0bfb9d452b246c92697b2eee/sample.json')
-    .then(res => {
-      setTitle(res.data.title)
-      setDescription(res.data.description)
-    })
+  useEffect(() => {
+    axios.get('https://gist.githubusercontent.com/bittermeatball/7854f3d7950469b0203a068fcaf27908/raw/1de87462c4f8c2fd0bfb9d452b246c92697b2eee/sample.json')
+      .then(res => {
+        setTitle(res.data.title)
+        setDescription(res.data.description)
+        setSectionNumber(res.data.sections.length)
+        setHasNext(res.data.sections.length > 1)
+      })
+  }, [])
+
+  useEffect(() => {
+    setHasPrevious(sectionId > 0)
+    setHasNext(sectionId < sectionNumber - 1)
+  }, [sectionId])
+
+  const handlePrevious = () => {
+    if (sectionId > 0) {
+      setSectionId(sectionId - 1)
+    }
+  }
+
+  const handleNext = () => {
+    if (sectionId < sectionNumber - 1) {
+      setSectionId(sectionId + 1)
+    }
+  }
 
   return (
     <div className='information-wrapper'>
@@ -19,8 +44,8 @@ function Information() {
         <div className='information-description'>{description}</div>
       </div>
       <div className='information-action'>
-        <button className='information-button'>Mục trước</button>
-        <button className='information-button'>Mục sau</button>
+        <button className={`information-button ${hasPrevious ? '' : 'none'}`} onClick={handlePrevious}>Mục trước</button>
+        <button className={`information-button ${hasNext ? '' : 'none'}`} onClick={handleNext}>Mục sau</button>
       </div>
     </div>
   )
