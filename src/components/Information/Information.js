fcc6b8955faae9e5ffa7098965621d70e820dfdf
Huynh Thi Khanh Linh <huynhthikhanhlinh2105@gmail.com>
2021-12-28 20:47:08 +0700

    connect api for information

--- src/components/Information/Information.js
+++ src/components/Information/Information.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Information.scss'
+import axios from 'axios'
 
 function Information() {
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+
+  axios.get('https://gist.githubusercontent.com/bittermeatball/7854f3d7950469b0203a068fcaf27908/raw/1de87462c4f8c2fd0bfb9d452b246c92697b2eee/sample.json')
+    .then(res => {
+      setTitle(res.data.title)
+      setDescription(res.data.description)
+    })
+
   return (
     <div className='information-wrapper'>
       <div className='information-container'>
-        <div className='information-title'>Form khảo sát tình hình học tập các thành viên GDSC - DUT</div>
-        <div className='information-description'>Sau một khoảng thời gian hoạt động, GDSC xin được khảo sát các bạn thành viên yêu mến về tình hình học tập và rèn luyện của các bạn tại GDSC.</div>
+        <div className='information-title'>{title}</div>
+        <div className='information-description'>{description}</div>
       </div>
       <div className='information-action'>
         <button className='information-button'>Mục trước</button>
