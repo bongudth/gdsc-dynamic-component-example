4cf530de81d5ffdc1fd4d49beff3cb264894dadb
Huynh Thi Khanh Linh <huynhthikhanhlinh2105@gmail.com>
2022-01-02 16:54:32 +0700

    change dependence of answersForm into sectionId

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -11,6 +11,14 @@ function Form(props) {
   const [questions, setQuestions] = useState([])
   const [answers, setAnswers] = useState([])
 
+  const usePrevious = value => {
+    const ref = React.useRef()
+    React.useEffect(() => {
+      ref.current = value
+    })
+    return ref.current
+  }
+
   useEffect(() => {
     axios.get('https://gist.githubusercontent.com/bittermeatball/7854f3d7950469b0203a068fcaf27908/raw/1de87462c4f8c2fd0bfb9d452b246c92697b2eee/sample.json')
       .then(res => {
@@ -22,26 +30,27 @@ function Form(props) {
     setSectionId(props.sectionId)
   }, [props.sectionId])
 
+  const sectionIdPrevious = usePrevious(sectionId)
+
   useEffect(() => {
     if (sections.length === 0) return
 
     setSectionTitle(sections[sectionId].title)
     setSectionDescription(sections[sectionId].description)
     setQuestions(sections[sectionId].questions)
-    setAnswers([])
+    setAnswers(props.answersSection ? props.answersSection : [])
+
+    if (answers.length === 0) return
+    props.setAnswersForm((answersForm) => {
+      answersForm[sectionIdPrevious] = [...JSON.parse(JSON.stringify(answers))]
+      return [...answersForm]
+    })
   }, [sectionId, sections])
 
   useEffect(() => {
     if (questions.length === 0) return
     props.setIsComplete(answers.filter(answer => answer === '').length === 0)
   }, [JSON.stringify(answers)])
-
-  useEffect(() => {
-    props.setAnswersForm((answersForm) => {
-      answersForm[sectionId] = answers
-      return [...answersForm]
-    })
-  }, [props.isComplete])
 
   return (
     <div className='form-container'>
