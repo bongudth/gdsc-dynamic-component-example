0a0d145570c25bf55ab0890ca540bc161b47112f
Huynh Thi Khanh Linh <huynhthikhanhlinh2105@gmail.com>
2022-01-02 15:41:06 +0700

    implement confetti

--- src/components/Confetti/Confetti.js
+++ src/components/Confetti/Confetti.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Confetti.scss'
+import { launchConfetti } from '../../utils/confetti'
 
 function Confetti(props) {
+  useEffect(() => {
+    if (props.isShow) launchConfetti()
+  }, [props.isShow])
+
   return (
     props.isShow && <div className='confetti-container'>Cảm ơn bạn đã điền form khảo sát!<br />Hãy mở cửa sổ console để xem kết quả!</div>
   )
